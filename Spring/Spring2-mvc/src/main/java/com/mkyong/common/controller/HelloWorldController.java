8f2ab5e8e7641289093f3203975e3b0d20f38dd0
erickjones2 <erick.jones@flexagon.com>
2022-05-25 16:57:18 -0500

    Fixed GLORY-53

--- Spring/Spring2-mvc/src/main/java/com/mkyong/common/controller/HelloWorldController.java
+++ Spring/Spring2-mvc/src/main/java/com/mkyong/common/controller/HelloWorldController.java
@@ -13,7 +13,7 @@ public class HelloWorldController extends AbstractController {
 			HttpServletResponse response) throws Exception {
 
 		ModelAndView model = new ModelAndView("HelloWorldPage");
-		model.addObject("msg", "hello ddd!");
+		model.addObject("msg", "hello eee!");
 
 		return model;
 	}
